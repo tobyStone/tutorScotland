d4559167e094c146a56d3ca23db867be645026bf
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2025-05-05 19:45:16 +0100

    object formidable

--- api/sections.js
+++ api/sections.js
@@ -3,7 +3,7 @@
  * Images go to Vercel Blob, meta to MongoDB.
  */
 const { put } = require('@vercel/blob');
-const formidable = require('formidable');
+const { formidable } = require('formidable');
 const fs = require('fs');
 const mongoose = require('mongoose');
 const connectToDB = require('./connectToDatabase');
