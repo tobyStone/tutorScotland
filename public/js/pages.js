30662b83c207c22dccc8c465bc18df1af2fc94d2
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2025-06-11 13:09:28 +0100

    auggie spread to other static pages of dynamic and edit

---
+++ public/js/pages.js
@@ -0,0 +1,12 @@
+export const PAGES = [
+  'index',
+  'about-us',
+  'contact',
+  'parents',
+  'tutorconnect',
+  'tutordirectory',
+  'tutormembership',
+  'tutorszone',
+  'partnerships',        // ★ NEW
+  'rolling-banner'
+];
