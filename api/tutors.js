0d219c92357782f0ed164f5af8b11e32c14a9fec
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2025-07-08 11:44:16 +0100

    ...

--- api/tutors.js
+++ api/tutors.js
@@ -245,7 +245,7 @@ module.exports = async (req, res) => {
                     .tutor-directory-page #imageBanner {
                         opacity: 0;
                         position: absolute;
-                        top: 18% !important; /* Position closer to shield bottom tip */
+                        top: calc(18% + 77px) !important; /* Position 77px lower than previous position */
                         left: 60% !important;
                         animation: growLeft 2s forwards ease-in-out;
                         animation-delay: 1.5s; /* ribbons appear after shield */
