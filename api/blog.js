8eab0187e06c385799ecc98ac276b318da5738c3
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2025-07-14 12:01:21 +0100

    typography css modularisation

--- api/blog.js
+++ api/blog.js
@@ -168,6 +168,7 @@ module.exports = async (req, res) => {
           <link rel="stylesheet" href="/styles2.css">
           <link rel="stylesheet" href="/css/footer-module.css">
           <link rel="stylesheet" href="/css/button-module.css">
+          <link rel="stylesheet" href="/css/typography-module.css">
           <link rel="stylesheet" href="/header-banner.css">
           <link rel="stylesheet" href="/css/nav.css">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
